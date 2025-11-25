f0c14119ed6671137bad11eef3148ec93b3dc921
Ankur Sardar <ankursardar.work@gmail.com>
2025-11-26 01:56:22 +0530

    farenheit to celcius

--- C Programming/Let US C/Chap 1/farenheitToCelcius.c
+++ C Programming/Let US C/Chap 1/farenheitToCelcius.c
@@ -3,6 +3,7 @@ int main(){
     int temp;
     printf("Enter the temperature(in Farenheit): ");
     scanf("%d",&temp);
-    
+    int celcius=(temp-32)/1.8;
+    printf("The temperature(in Celcius): %d\n",celcius);
     return 0;
 }
