f61a419ef20d14d74645c08222b072463114ff58
Ankur Sardar <ankursardar.work@gmail.com>
2025-12-02 17:19:00 +0530

    testing another code

--- C Programming/Extras/test.c
+++ C Programming/Extras/test.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main(){
-    int i=54;
+    // int i=54;
     float a=3.14;
-    char *ii, *aa;
-    ii = &i;
-    aa = &a;
-    printf("%p\n",ii);
-    printf("%p\n",aa);
-    printf("%d\n",*ii);
-    printf("%d\n",*aa);
+    float *b;
+    b=&a;
+    // char *ii, *aa;
+    // ii = &i;
+    // aa = &a;
+    // printf("%p\n",ii);
+    // printf("%p\n",aa);
+    printf("%d\n",*b);
+    // printf("%d\n",*aa);
     return 0;
 }
