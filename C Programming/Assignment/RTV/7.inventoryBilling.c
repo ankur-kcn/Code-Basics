97aededdcf6989ecf223a4d50b54fbc9fd9c31e0
Ankur Sardar <ankursardar.work@gmail.com>
2025-12-06 10:21:22 +0530

    inventory done

--- C Programming/Assignment/RTV/7.inventoryBilling.c
+++ C Programming/Assignment/RTV/7.inventoryBilling.c
@@ -7,18 +7,38 @@
 // Real use: Grocery store billing prototype.
 
 #include<stdio.h>
-
-int main(){
-    char name[10][50];
-    int qty;
-    
-    printf("----- List of %d Items -----\n",10);
-    for(int i=0;i<10;i++){
-        printf("Item %d: ",i+1);
-        fgets(name[i],sizeof(name[i]),stdin);
-        printf("Quantity : ");
-        scanf("%d",&qty);
-        getchar();
+#define items 10
+float calCost(float *p,int *q){
+    return (*p)*(*q);
+}
+float calTotal(float *p,int *q,int n){
+    float sum=0;
+    for(int i=0;i<items;i++){
+        sum+= *(p+i) * *(q+i);
     }
+    return sum;
+}
+int main(){
+    float price[items];
+    int qty[items];
+    
+    printf("----- List of %d Items -----\n",items);
+    for(int i=0;i<items;i++){
+        printf("Item %d: ₹",i+1);
+        scanf("%f",&price[i]);
+        printf("Quantity : ");
+        scanf("%d",&qty[i]);
+    }
+    printf("\n---------- Inventory Billing ----------\n");
+    printf("Item\t | Quantity \t| Cost \n");
+    for(int i=0;i<10;i++){
+        float cost=calCost(&price[i],&qty[i]);
+        printf("%d. ₹%.2f\t%d\t%.2f\n",i+1,price[i],qty[i],cost);
+    }
+    float Sum=0;
+        Sum=calTotal(price,qty,items);
+    printf("----------------------------------------\n");
+    printf("Total Billing Amount : %.2f\n",Sum);
+    printf("----------------------------------------\n");
     return 0;
 }
