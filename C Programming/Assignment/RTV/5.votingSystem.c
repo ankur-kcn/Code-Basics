c83f4b1217f8ada855a0f9362bb2c38396986258
Ankur Sardar <ankursardar.work@gmail.com>
2025-12-06 15:53:10 +0530

    Atm system done

---
+++ C Programming/Assignment/RTV/5.votingSystem.c
@@ -0,0 +1,7 @@
+// 5. Voting System (Loops + 1D Array Counter)
+// Features:
+// • Take votes for 5 candidates
+// • Store votes in 1D integer array
+// • Find winner using loops
+// • Function for counting & result display
+// Real use: Simple election vote counter.
